2a0c8b47b466a49e6320a2248195352b7d3f53f6
pan奎 <nbapankui@163.com>
2021-11-11 20:56:10 +0800

    初始化项目

---
+++ graalvm-example/src/main/java/example/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package example.helloworld;
+
+/**
+ * @author pankui
+ * @date 2021/11/11 
+ * <pre>
+ *    项目执行：
+ *    mvn package
+ *    ./target/graalvm-example
+ *
+ *    https://blog.csdn.net/liujinghu/article/details/114199071
+ * </pre>  
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world graalvm");
+    }
+}
